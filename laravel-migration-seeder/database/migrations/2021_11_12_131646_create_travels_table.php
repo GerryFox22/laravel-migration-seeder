3852e489b900f4e1295bafb6c2dd1fcb69666fb1
GerryFox22 <gerry.cuomo1994@gmail.com>
2021-11-12 14:33:43 +0100

    inizializzazione laravel-migration-seeder

---
+++ laravel-migration-seeder/database/migrations/2021_11_12_131646_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('image');
+            $table->integer('price');
+            $table->string('destination');
+            $table->integer('how_many_people');
+            $table->date('date');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
